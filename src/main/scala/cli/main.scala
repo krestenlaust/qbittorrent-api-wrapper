8051b51f10fca9edcd8ed31f32811f3bc3803ef7
Kresten Laust <krestenlaust@gmail.com>
2024-01-29 10:41:46 +0100

    Changed debug output

--- src/main/scala/cli/main.scala
+++ src/main/scala/cli/main.scala
@@ -4,4 +4,4 @@ import qbittorrentapi.qBitTorrentApi
 
 @main def main() =
   val torrentApi = qBitTorrentApi("http://127.0.0.1:8080/")
-  torrentApi.connect("admin", "123123")
+  println(torrentApi.torrents.info())
