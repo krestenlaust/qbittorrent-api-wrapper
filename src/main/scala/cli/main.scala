33f3fc6f89eaba6ba19c2ecdfa5919c7a38a5912
Kresten Laust <krestenlaust@gmail.com>
2024-02-03 00:06:52 +0100

    Working example in main

--- src/main/scala/cli/main.scala
+++ src/main/scala/cli/main.scala
@@ -5,13 +5,15 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.*
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.server.Route.seal
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import qbittorrentapi.qBitTorrentApi
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
 
 @main def main() =
   implicit val system = ActorSystem("qbittorrent-api-system")
   implicit val executionContext: ExecutionContext = system.dispatcher
 
-  val torrentApi = qBitTorrentApi("http://127.0.0.1:8080/", None)(system, executionContext)
-  println(torrentApi.torrents.info())
+  val torrentApi = qBitTorrentApi("http://127.0.0.1:8080", None)(system, executionContext)
+  println(Unmarshal(Await.result(torrentApi.torrents.info(), Duration.Inf).entity).to[String])
